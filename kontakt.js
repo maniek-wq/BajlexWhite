db6418e9c61b8c53e62a34019cfcc9c8789f68e6
unknown <jakwasi01@gmail.com>
2023-05-29 00:37:03 +0200

    resp

--- kontakt.js
+++ kontakt.js
@@ -6,18 +6,6 @@ link.addEventListener("click",function(){
 });
 
 
-const nav = document.getElementById("navbar");
-window.addEventListener("scroll", function() {
-  if (window.pageYOffset > 0) {
-    nav.style.opacity=".85";
-    nav.classList.add("shadow");
-    // console.log("sie ruszylo");
-  } else {
-    nav.style.opacity="1";
-    nav.classList.remove("shadow");
-    // console.log("w miejscu")
-  }
-});
 
 
 
@@ -125,3 +113,18 @@ if(mediaQuery.matches){
                 }
         });
 }
+
+
+const nav = document.getElementById("navbar");
+window.addEventListener("scroll", function() {
+  if (window.pageYOffset > 0) {
+    nav.style.opacity=".85";
+    nav.classList.add("shadow");
+    nav.style.zIndex="999";
+    // console.log("sie ruszylo");
+  } else {
+    nav.style.opacity="1";
+    nav.classList.remove("shadow");
+    // console.log("w miejscu")
+  }
+});
