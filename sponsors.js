65e4737dfe2b9bca8e98e240c063136a6374fc8a
unknown <jakwasi01@gmail.com>
2023-06-15 11:47:26 +0200

    horizontal 812px

--- sponsors.js
+++ sponsors.js
@@ -84,3 +84,16 @@ if(mediaQuery.matches){
                 }
         });
 }
+
+const nav = document.getElementById("navbar");
+window.addEventListener("scroll", function() {
+  if (window.pageYOffset > 0) {
+    nav.style.opacity=".85";
+    nav.classList.add("shadow");
+    // console.log("sie ruszylo");
+  } else {
+    nav.style.opacity="1";
+    nav.classList.remove("shadow");
+    // console.log("w miejscu")
+  }
+});
